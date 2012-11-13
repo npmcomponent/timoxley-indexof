09e1f5fd3d45c46a0d37e1689683f62df1d62eb1
Tim Oxley <secoif@gmail.com>
2012-11-13 19:14:42 +1000

    Use timoxley-assert over component/assert.

--- test/indexof.js
+++ test/indexof.js
@@ -1,4 +1,4 @@
-var assert = require('component-assert')
+var assert = require('timoxley-assert')
 var domify = require('component-domify')
 var indexOf = require('indexof')
 
@@ -11,11 +11,11 @@ it('should return -1 element has no parent', function() {
 it('should return 0 if first in list', function() {
   var dom = domify('<ul><li>1</li></ul>')
   var el = dom.children[0]
-  assert(indexOf(el) === 0)
+  assert.strictEqual(indexOf(el), 0)
 })
 
 it('should return 1 if second in list', function() {
   var dom = domify('<ul><li>1</li><li>2</li></ul>')
   var el = dom.children[1]
-  assert(indexOf(el) === 1)
+  assert.strictEqual(indexOf(el), 1)
 })
