9487a43f0310a082eed24d365e88f94aeb91aa39
Tim Oxley <secoif@gmail.com>
2012-11-24 15:59:18 +1000

    Fix for domify return signature change.

--- test/indexof.js
+++ test/indexof.js
@@ -3,19 +3,19 @@ var domify = require('component-domify')
 var indexOf = require('indexof')
 
 it('should return -1 element has no parent', function() {
-  var el = domify('<ul></ul>')
+  var el = domify('<ul></ul>')[0]
   el = el.parentNode && el.parentNode.removeChild(el) // bugfix for domify
   assert(indexOf(el) === -1)
 })
 
 it('should return 0 if first in list', function() {
-  var dom = domify('<ul><li>1</li></ul>')
+  var dom = domify('<ul><li>1</li></ul>')[0]
   var el = dom.children[0]
   assert.strictEqual(indexOf(el), 0)
 })
 
 it('should return 1 if second in list', function() {
-  var dom = domify('<ul><li>1</li><li>2</li></ul>')
+  var dom = domify('<ul><li>1</li><li>2</li></ul>')[0]
   var el = dom.children[1]
   assert.strictEqual(indexOf(el), 1)
 })
